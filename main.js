803b2ad417e842e341bb900e7ddbe09de910d253
Ed Ive <ed.ivve@gmail.com>
2019-04-24 21:28:31 +0100

    streamline json and main.js, readying to incorporate verses illustration

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// CAROUSEL BEGINS
 let sources = [
     "img/1.jpg",
     "img/2.jpg",
@@ -10,25 +11,39 @@ let sources = [
     "img/9.jpg",
     "img/10.jpg"
 ];
-
-
 function assign() {
     img0.src = sources[0];
     img1.src = sources[1];
     img2.src = sources[2];
 }
-
 let img0 = document.getElementById("img0");
 let img1 = document.getElementById("img1");
 let img2 = document.getElementById("img2");
-
 assign();
-
 img0.addEventListener("click", () => {
     carouselFwd(sources);
 });
-
 function carouselFwd(arr) {
     arr.push(arr.shift());
     assign();
 }
+// CAROUSEL ENDS
+
+// VERSES BEGIN
+let verses, titles;
+
+function loadDataset(requestURL) {
+  let request = new XMLHttpRequest();
+  request.open('GET', requestURL);
+  console.log("works");
+  request.responseType = 'json';
+  request.send();
+  request.onload = function() {
+    let res = request.response;
+    console.log("res loaded", res);
+    verses = res.verses;
+    titles = res.titles;
+  }
+}
+
+loadDataset("txt/verses.json");
