c2cee19d47a18d55075ce04c68a2789d12f707f6
Ed Ive <ed.ivve@gmail.com>
2019-05-01 01:47:20 +0100

    minify sources for img

--- main.js
+++ main.js
@@ -1,36 +1,5 @@
 // CAROUSEL BEGINS
-let sources = [
-    "img/1.jpg",
-    "img/2.jpg",
-    "img/3.jpg",
-    "img/4.jpg",
-    "img/5.jpg",
-    "img/6.jpg",
-    "img/7.jpg",
-    "img/8.jpg",
-    "img/9.jpg",
-    "img/10.jpg",
-    "img/11.jpg",
-    "img/12.jpg",
-    "img/13.jpg",
-    "img/14.jpg",
-    "img/15.jpg",
-    "img/16.jpg",
-    "img/17.jpg",
-    "img/20.jpg",
-    "img/21.jpg",
-    "img/22.jpg",
-    "img/23.jpg",
-    "img/24.jpg",
-    "img/25.jpg",
-    "img/26.jpg",
-    "img/27.jpg",
-    "img/28.jpg",
-    "img/29.jpg",
-    "img/30.jpg",
-    "img/vierkant.jpg",
-    "img/sekula.jpg",
-    "img/rozendaal.gif"
+let sources = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg", "img/6.jpg", "img/7.jpg", "img/8.jpg", "img/9.jpg", "img/10.jpg", "img/11.jpg", "img/12.jpg", "img/13.jpg", "img/14.jpg", "img/15.jpg", "img/16.jpg", "img/17.jpg", "img/20.jpg", "img/21.jpg", "img/22.jpg", "img/23.jpg", "img/24.jpg", "img/25.jpg", "img/26.jpg", "img/27.jpg", "img/28.jpg", "img/29.jpg", "img/30.jpg", "img/vierkant.jpg", "img/sekula.jpg", "img/rozendaal.gif"
 ];
 function assign() {
     img0.src = sources[0];
